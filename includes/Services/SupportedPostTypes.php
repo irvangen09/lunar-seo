df08691d2411295111ec971237da7bd4d46e86a4
nymphadora07 <cocnymphadora@gmail.com>
2026-09-11 20:13:03 +0700

    feat: add SupportedPostTypes shared service for lunar_seo_supported_post_types filter

---
+++ includes/Services/SupportedPostTypes.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Lunar\SEO\Services;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+final class SupportedPostTypes {
+
+	private const DEFAULT = [
+		'post' => [
+			'content_group' => 'post',
+			'schema_node'   => 'article',
+		],
+		'page' => [
+			'content_group' => 'page',
+			'schema_node'   => 'webpage',
+		],
+	];
+
+	public function all(): array {
+		return apply_filters( 'lunar_seo_supported_post_types', self::DEFAULT );
+	}
+
+	public function is_supported( string $post_type ): bool {
+		return isset( $this->all()[ $post_type ] );
+	}
+
+	public function content_group( string $post_type ): ?string {
+		return $this->all()[ $post_type ]['content_group'] ?? null;
+	}
+
+	public function schema_node( string $post_type ): ?string {
+		return $this->all()[ $post_type ]['schema_node'] ?? null;
+	}
+}
